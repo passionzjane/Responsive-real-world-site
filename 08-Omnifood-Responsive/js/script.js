00f7fce76042536eea1b1aaf1d25d304b6dd0725
passionzjane <mrumunjanet@gmail.com>
2023-07-25 11:39:33 +0100

    Smooth scrolling implimented

--- 08-Omnifood-Responsive/js/script.js
+++ 08-Omnifood-Responsive/js/script.js
@@ -27,6 +27,34 @@ btnNavEl.addEventListener("click", function() {
   headerEL.classList.toggle("nav-open");
 })
 
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const alllinks = document.querySelectorAll("a:link")
+
+alllinks.forEach(function(link) {
+  link.addEventListener("click", function(e) {
+    e.preventDefault()
+    const href = link.getAttribute('href');
+
+    // Scroll back to top
+    if (href === "#") window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+
+    //Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href)
+      sectionEl.scrollIntoView({ behavior: "smooth"})
+    }
+
+    //Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEL.classList.toggle("nav-open")
+  });
+});
+
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
