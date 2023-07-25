955ab1e11d08c72655328e64629c841c5addfe90
passionzjane <mrumunjanet@gmail.com>
2023-07-25 10:23:17 +0100

    mobile navigation completed

--- 08-Omnifood-Responsive/js/script.js
+++ 08-Omnifood-Responsive/js/script.js
@@ -12,9 +12,21 @@ console.log(h1);
 //   h1.style.padding = "5rem"
 // });
 
+///////////////////////////////////////////////////////////
+//Set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+//Make mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEL = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function() {
+  headerEL.classList.toggle("nav-open");
+})
+
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
